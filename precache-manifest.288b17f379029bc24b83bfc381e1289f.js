814100d04a82e6a9b7c76e19615d7aed9e537f1a
Jaime Burbano <burbanojaime98@gmail.com>
2019-07-12 17:28:07 -0500

    Updates

---
+++ precache-manifest.288b17f379029bc24b83bfc381e1289f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d45065dff5649d6310ebc78b46426157",
+    "url": "/Reto-Unicornio/index.html"
+  },
+  {
+    "revision": "754d6d368c24452421ac",
+    "url": "/Reto-Unicornio/static/css/main.23f3076b.chunk.css"
+  },
+  {
+    "revision": "c5422d6f594289a3bde3",
+    "url": "/Reto-Unicornio/static/js/2.8be7fa71.chunk.js"
+  },
+  {
+    "revision": "754d6d368c24452421ac",
+    "url": "/Reto-Unicornio/static/js/main.5f76e91d.chunk.js"
+  },
+  {
+    "revision": "bc908ccdd0abbece1efe",
+    "url": "/Reto-Unicornio/static/js/runtime~main.1aa296b2.js"
+  }
+]);
