c186d74b43db87ff0712a87e89514bbdc0aba2bb
Jaime Burbano <burbanojaime98@gmail.com>
2019-07-13 15:53:12 -0500

    a

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import HomeLayout from './HomeLayout';
-import Rocket from '../../animations/rocket/Rocket';
 
 export default class Home extends React.Component{
     render(){
