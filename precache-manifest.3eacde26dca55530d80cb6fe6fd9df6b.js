071c1aa258f3766a841dc1f69221d017f4268a12
Jaime Burbano <burbanojaime98@gmail.com>
2019-07-13 17:17:18 -0500

    Updates

---
+++ precache-manifest.3eacde26dca55530d80cb6fe6fd9df6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a52d6311f17e5acd5c6d93f7e13a9b9d",
+    "url": "/Reto-Unicornio/index.html"
+  },
+  {
+    "revision": "d444a9783dae815bf24c",
+    "url": "/Reto-Unicornio/static/css/main.83ae052f.chunk.css"
+  },
+  {
+    "revision": "4f3036dee84a5cd20fe7",
+    "url": "/Reto-Unicornio/static/js/2.aa053a1f.chunk.js"
+  },
+  {
+    "revision": "d444a9783dae815bf24c",
+    "url": "/Reto-Unicornio/static/js/main.194930ad.chunk.js"
+  },
+  {
+    "revision": "bc908ccdd0abbece1efe",
+    "url": "/Reto-Unicornio/static/js/runtime~main.1aa296b2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Reto-Unicornio/precache-manifest.78d6dc746daef7576f94bed4aa3b466b.js"
+  "/Reto-Unicornio/precache-manifest.3eacde26dca55530d80cb6fe6fd9df6b.js"
 );
 
 self.addEventListener('message', (event) => {
